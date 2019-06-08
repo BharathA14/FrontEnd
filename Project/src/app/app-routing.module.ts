3ef8fc58bfc8d793a77881839327dd9887e0e493
BharathA14 <bbharath201@gmail.com>
2019-06-08 17:49:54 +0530

    Update login(Fixed)

--- Project/src/app/app-routing.module.ts
+++ Project/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule, Route } from '@angular/router';
 import { SprintComponent } from './sprint/sprint.component';
 import {ProfileComponent} from './profile/profile.component';
 import { CreateIssueComponent } from './create-issue/create-issue.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Route[]= [
   {path:'sprint',component:SprintComponent,
@@ -11,6 +12,7 @@ children:[
   
 ]},
   {path:'profile', component: ProfileComponent},
+  {path:'login', component:LoginComponent}
 ]
 
 @NgModule({
